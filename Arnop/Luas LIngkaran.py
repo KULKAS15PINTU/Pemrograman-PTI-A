c8b15230210e5eb97a6618505330ea574ba92b66
Kulkas  15 Pintu <arnop817@gmail.com>
2024-11-10 04:03:50 -1200

    Add files via upload

---
+++ Arnop/Luas LIngkaran.py
@@ -0,0 +1,10 @@
+import math
+
+# Masukan nilai jari-jari
+r = 10
+
+# Menghitung luas lingkaran dengan nilai pi dari modul math
+luas_lingkaran = math.pi * (r ** 2)
+
+# Menampilkan hasil
+print("Luas lingkaran dengan jari-jari", r, "adalah:", round(luas_lingkaran, 2))
