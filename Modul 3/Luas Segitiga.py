97bfb03f102f37f65f34eb93244de7003e4c5aec
Kulkas  15 Pintu <arnop817@gmail.com>
2024-11-10 14:15:38 -1200

    up ulang

---
+++ Modul 3/Luas Segitiga.py
@@ -0,0 +1,9 @@
+# Meminta input dari user
+alas = int(input("masukkan panjang alas segitiga: "))
+tinggi = float(input("masukkan tinggi segitiga: "))
+
+# Menghitung luas segitiga
+luas_segitiga = 0.5 * alas * tinggi
+
+# Menampilkan hasil
+print("Luas segitiga adalah:", luas_segitiga)
