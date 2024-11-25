329b1810659605cde77a578b34b8f0f8dd2ffd7c
Kulkas  15 Pintu <arnop817@gmail.com>
2024-11-24 16:27:18 -1200

    up ulang

---
+++ Modul 4/10-PengenalanString/main.py
@@ -0,0 +1,35 @@
+# Mengenal string 
+# string = kumpulan karakter
+
+data = "ini adalah string"
+print(data)
+print(type (data))
+
+# 1. cara membuat string
+"""
+   1. dengan menggunakan single quote '..'
+   2. dengan menggunakan double quote ".."
+"""
+
+data = 'Menggunakan Single Quote'
+print(data)
+
+data = "Menggunakan Single Quote"
+print(data)
+
+print("ini hari jum'at")
+print("nama saya ucup")
+
+# 2. menggunakan tanda \
+
+# membuat tanda ' menjadi string
+print('mari sholat jum\'at')
+
+print('c:\\user\\ucup')
+
+# tab
+print("ronaldo\t\tmessi") # (\t) untuk tab atau spasi
+print("MU\t\tJuara")
+
+# backspace
+print("MU\b\bJuara")
